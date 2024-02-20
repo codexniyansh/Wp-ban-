e39a43772709b543c3d90c9303563e99b7c77345
Niyansh Jain <156841173+codexniyansh@users.noreply.github.com>
2024-02-20 13:45:07 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,38 @@
+r = "\033[1;31m"
+g = "\033[1;32m"
+y = "\033[1;33m"
+b = "\033[1;34m"
+d = "\033[2;37m"
+R = "\033[1;41m"
+Y = "\033[1;43m"
+B = "\033[1;44m"
+w = "\033[1;37m"
+
+
+
+
+logo = """\033[1;37m
+
+┌-=============================   -   
+=      \033[1;31m . ┌──────── \033[1;37mVaim-Emsg     -   
+=  \033[1;31m .┌───  \033[1;37mB-Hat Thor PROJECT   -   \033[34m[✔️] https://github.com/codexniyansh/Wp-ban-.git    [✔️]
+\033[1;37m=   Wp ban           -   \033[34m[✔️]            Version 1.0                 [✔️]
+\033[1;37m= \033[1;31m . └──── \033[1;37mBY Thor   -   \033[91m[X] Please Don't Use For illegal Activity  [X]
+\033[1;37m= \033[1;31m .     └─── \033[1;37mVERSION 1.0         -
+\033[1;37m└-=============================   -
+
+
+
+\033[97m """
+
+
+
+def banner():
+    print(logo)
+
+
+banner()
+
 import requests
 code=input("ENTER A CODE ")
 NUMBER=input("ENTER A NUMBER ")
